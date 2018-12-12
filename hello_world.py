d5c98ca3f0b09cf87eff97faf6cb42ba246d68cb
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2018-12-12 10:02:40 +0000

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# Scrolls the text "Hello, World" across the LED display of the microbit
+
+from microbit import *
+display.scroll("Hello, World!")
+
+
+# Try the following lines to see what happens
+
+display.scroll("Hello, World!", delay=150) 
+display.scroll("Hello, World!", loop=False)
