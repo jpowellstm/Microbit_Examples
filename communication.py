f7ddae21fd96443af7b2ae67811b6578e6e3a502
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2018-12-12 14:34:56 +0000

    Create communication.py

---
+++ communication.py
@@ -0,0 +1,35 @@
+# Some examples to demonstrate how the micro:bits can communicate with each other
+
+# Example 1 - basic communication
+
+import radio
+from microbit import *
+
+radio.on()
+
+while True:
+    if button_a.was_pressed():
+        radio.send('hello') 
+   
+    incoming = radio.receive()
+    if incoming == 'hello':
+        sleep(500)
+        radio.send('world')
+        
+# Example 2 - Communication with channel and power settings
+
+import radio
+from microbit import *
+
+radio.on()
+radio.config(channel=19)
+radio.config(power=7)
+
+while True:
+    if button_a.was_pressed():
+        radio.send('hello') 
+   
+    incoming = radio.receive()
+    if incoming == 'hello':
+        sleep(500)
+        radio.send('world')
