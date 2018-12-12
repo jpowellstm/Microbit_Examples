e31157ef6a43096673d7dc55edc3da7af0316691
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2018-12-12 14:52:41 +0000

    Create morethings.py

---
+++ morethings.py
@@ -0,0 +1,16 @@
+# random numbers
+
+import random
+
+# generate a random number between 5 and 50
+number = random.randint(5,50)
+
+
+# casting
+
+# convert and integer to a string
+str(5)
+
+# convert a string to an integer
+int("5")
+
