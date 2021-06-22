484e21a3760c7a55a0edbfed575ceb2d47548ae1
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2021-06-22 13:16:54 +0100

    Update bit_bot.py
    Added PWM speed control functions

--- bit_bot.py
+++ bit_bot.py
@@ -8,8 +8,7 @@ def lft_bck():
   #Move left motor reverse full speed
   pin0.write_digital(0)
   pin8.write_digital(1)
-  
-  
+    
 def rgt_fwd():
   #Move right motor forward full speed
   pin1.write_digital(1)
@@ -18,5 +17,25 @@ def rgt_fwd():
 def rgt_bck():
   #Move right motor reverse full speed
   pin1.write_digital(0)
+  pin12.write_digital(1)
+  
+def lft_fwd_speed(speed):
+  #Move left motor forward at a speed between 0 and 1024
+  pin0.write_analog(speed)
+  pin8.write_digital(0)
+  
+def rgt_fwd_speed(speed):
+  #Move right motor forward at a speed between 0 and 1024
+  pin1.write_analog(speed)
+  pin12.write_digital(0)
+
+def lft_bck_speed(speed):
+  #Move left motor backward at a speed between 0 and 1024 
+  pin0.write_analog(1024-speed)
+  pin8.write_digital(1)  
+
+def rgt_bck_speed(speed):
+  #Move right motor backward at a speed between 0 and 1024 
+  pin1.write_analog(1024-speed)
   pin12.write_digital(1)
   
