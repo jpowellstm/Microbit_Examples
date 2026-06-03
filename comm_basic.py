f066a1d7dc0e4be80389a1d7b83801ff8fb6d294
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2026-06-03 13:14:07 +0100

    Update and rename communication.py to comm_basic.py

---
+++ comm_basic.py
@@ -0,0 +1,19 @@
+""" A basic examples to demonstrate how the micro:bits can communicate with each other
+"""
+
+import radio
+from microbit import *
+
+radio.on()
+
+while True:
+    if button_a.was_pressed():
+        radio.send('hello') 
+   
+    incoming = radio.receive()
+    if incoming == 'hello':
+        sleep(500)
+        radio.send('world')
+    if incoming is not None:   
+        display.scroll(incoming)
+    
