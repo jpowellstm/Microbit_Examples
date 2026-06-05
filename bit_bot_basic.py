b90aeeb46161a3b55d51ed094049cb5cde73b3d5
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2026-06-05 09:28:51 +0100

    Update bit_bot_basic.py

--- bit_bot_basic.py
+++ bit_bot_basic.py
@@ -16,6 +16,16 @@ def rgt_fwd_speed(speed):
   pin1.write_analog(speed)
   pin12.write_digital(0)
 
+def lft_bck_speed(speed):
+  #Move left motor backward at a speed between 0 and 1024 
+  pin0.write_analog(speed)
+  pin8.write_digital(1)  
+
+def rgt_bck_speed(speed):
+  #Move right motor backward at a speed between 0 and 1024 
+  pin1.write_analog(speed)
+  pin12.write_digital(1)
+  
 def forward(speed):
   # drive forward
   rgt_fwd_speed(speed)
